c53b58ccb975f4f32dc7244ab94a83855d09db91
Tikhomirov <tikhomirov_ilya@mail.ru>
2022-11-17 16:43:48 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using _931920.tikhomirov.ilya.lab11.Models;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ICalcService, CalcService>();
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
+public interface ICalcService
+{
+  CalcData data { get; }
+}
+public class CalcService : ICalcService
+{    
+    public CalcData data => new CalcData();
+}
